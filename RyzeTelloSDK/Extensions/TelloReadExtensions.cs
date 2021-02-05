29389ee9699746775011f9c42e122d71aa7515a8
Andrei <gensurkin@yandex.ru>
2021-02-05 05:27:08 +0300

    sdk

---
+++ RyzeTelloSDK/Extensions/TelloReadExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace RyzeTelloSDK.Extensions
+{
+    public static class TelloReadExtensions
+    {
+        // ToDo: speed?
+        // ToDo: battery?
+        // ToDo: time?
+        // ToDo: wifi?
+        // ToDo: sdk?
+        // ToDo: sn?
+    }
+}
---
+++ RyzeTelloSDK/Extensions/TelloSetExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace RyzeTelloSDK.Extensions
+{
+    public static class TelloSetExtensions
+    {
+        // ToDo: set speed
+        // ToDo: set rc
+        // ToDo: set wifi ssid pass
+        // ToDo: set mon
+        // ToDo: set moff
+        // ToDo: set mdirection x
+        // ToDo: set ap ssid pass
+    }
+}
